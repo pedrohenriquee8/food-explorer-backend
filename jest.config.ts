1a7510c8a7e1054cb863dea88b2d3bc538e558f5
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2023-11-14 16:12:31 -0300

    chore: Add Jest configuration file (#2)

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+export default {
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+} as Config;
